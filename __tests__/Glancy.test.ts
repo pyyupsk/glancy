a18bff72f2b613e91bba684fc2e1fb5a574fc2ef
pyyupsk <pyyupsk@proton.me>
2025-01-25 13:25:17 +0700

    test: simplify localStorage mock in Glancy tests

--- __tests__/Glancy.test.ts
+++ __tests__/Glancy.test.ts
@@ -5,31 +5,7 @@ describe('Glancy Storage Library', () => {
   let storage: Glancy;
 
   beforeEach(() => {
-    Object.defineProperty(window, 'localStorage', {
-      value: {
-        store: {},
-        getItem(key: string | number) {
-          return this.store[key] || null;
-        },
-        setItem(key: string | number, value: { toString: () => unknown }) {
-          this.store[key] = value.toString();
-        },
-        clear() {
-          this.store = {};
-        },
-        removeItem(key: string | number) {
-          delete this.store[key];
-        },
-        get length() {
-          return Object.keys(this.store).length;
-        },
-        key(index: number) {
-          return Object.keys(this.store)[index] || null;
-        },
-      },
-      writable: true,
-    });
-
+    localStorage.clear();
     storage = new Glancy();
   });
 
